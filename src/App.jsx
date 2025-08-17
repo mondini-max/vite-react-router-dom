ea9156df4986f3f0c906fbdc68f66aae406dbdcf
Maxime Mondini <mh-mondini@hotmail.com>
2025-08-17 19:11:46 +0200

    Adds a basic error page and navigation
    Implements a catch-all error page to handle undefined routes, improving user experience.
    
    Also, introduces a basic navigation structure with a navbar and footer.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Products from './pages/Products.jsx';
+import Error from './pages/Error.jsx';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='products' element={<Products />} />
-      <Route />
-    </Routes>
+    <Fragment>
+      <nav>Navbar</nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <footer>footer</footer>
+    </Fragment>
   );
 }
 
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>404</h2>
+      <span>Page not Found</span>
+      <Link to='/' className='btn'>
+        back to home
+      </Link>
     </section>
   );
 };
